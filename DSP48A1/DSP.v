69665d66ac6027b0e73920fa07d18a41356392b7
ahmedmohamed0123 <ahmdmohamed1p@gmail.com>
2025-07-28 18:00:47 +0300

    edit file

--- DSP48A1/DSP.v
+++ DSP48A1/DSP.v
@@ -1,5 +1,5 @@
 module DSP48A1(A,B,D,C,CLK,CARRYIN,OPMODE,BCIN,RSTA,RSTB,RSTM,RSTP,RSTC,RSTD,RSTCARRYIN,RSTOPMODE,CEA,CEB
-,CEM,CEP,CEC,CED,CECARRYIN,CEOPMODE,PCIN,BCOUT,PCOUT,P,M,CARRYOUT,CARRYOUTF,BCIN);
+,CEM,CEP,CEC,CED,CECARRYIN,CEOPMODE,PCIN,BCOUT,PCOUT,P,M,CARRYOUT,CARRYOUTF);
 //Parameters 
  parameter A0REG=0; // number of pipeline registers in path for A =0
  parameter A1REG=1; //  number of pipeline registers in path for A =1
@@ -48,86 +48,76 @@ module DSP48A1(A,B,D,C,CLK,CARRYIN,OPMODE,BCIN,RSTA,RSTB,RSTM,RSTP,RSTC,RSTD,RST
  input RSTOPMODE; //reset for OPMODE registers
  input RSTP; //reset for P registers
  //Cascade Ports 
- output BCOUT;
- input PCIN;
- output PCOUT;
+ output [17:0] BCOUT;
+ input [47:0] PCIN;
+ output [47:0] PCOUT;
  // Design of DSP
- parameter Width_OPMODE=8;
   wire[7:0] opmodemux_out;
-  pipeline_mux  #(.Width(Width_OPMODE),.RSTTYPE(RSTTYPE)) opmode(.in(OPMODE),.clk(CLK),.out(opmodemux_out)
+  pipeline_mux  #(.Width(8),.RSTTYPE(RSTTYPE)) opmode(.in(OPMODE),.clk(CLK),.out(opmodemux_out)
  ,.CE(CEOPMODE),.rst(RSTOPMODE),.sel(OPMODEREG));
-  
-  parameter Width_D=18;
+
   wire [17:0] Dmux_out;
-  pipeline_mux  #(.Width(Width_D),.RSTTYPE(RSTTYPE)) D_mux(.in(D),.clk(CLK),.out(Dmux_out)
+  pipeline_mux  #(.Width(18),.RSTTYPE(RSTTYPE)) D_mux(.in(D),.clk(CLK),.out(Dmux_out)
  ,.CE(CED),.rst(RSTD),.sel(DREG));
 
- parameter Width_B=18;
  wire [17:0] B0_in;
- assign B0_in=(B_INPUT=="DIRECT")? B : BCIN; // input for sirst stage before B0mux
+ assign B0_in=(B_INPUT=="DIRECT")? B : (B_INPUT=="CASCCADE")? BCIN : 0; // input for sirst stage before B0mux
  wire[17:0] B0mux_out; // output for mux before pre adder substractor
-pipeline_mux  #(.Width(Width_B),.RSTTYPE(RSTTYPE)) B0_mux(.in(B0_in),.clk(CLK),.out(B0mux_out)
+pipeline_mux  #(.Width(18),.RSTTYPE(RSTTYPE)) B0_mux(.in(B0_in),.clk(CLK),.out(B0mux_out)
  ,.CE(CEB),.rst(RSTB),.sel(B0REG)); 
  
  wire [17:0] ADDER1;// output from pre adder substractor
  assign ADDER1= (opmodemux_out[6])? Dmux_out - B0mux_out : Dmux_out + B0mux_out;
 
- parameter Width_A=18;
+
  wire [17:0] A0mux_out;// output from first stage for A input
- pipeline_mux #(.Width(Width_A),.RSTTYPE(RSTTYPE)) A0_mux(.in(A),.clk(CLK),.out(A0mux_out),.CE(CEA),
+ pipeline_mux #(.Width(18),.RSTTYPE(RSTTYPE)) A0_mux(.in(A),.clk(CLK),.out(A0mux_out),.CE(CEA),
  .rst(RSTA),.sel(A0REG));
 
  wire [17:0] mux_OPMODED4;// output for mux after pre adder substactor
  assign mux_OPMODED4=(opmodemux_out[4])? ADDER1 : B0mux_out;
  wire [17:0] B1mux_out;//output for second stage after pre adder substractor
- pipeline_mux #(.Width(Width_B),.RSTTYPE(RSTTYPE)) B1_mux(.in(mux_OPMODED4),.clk(CLK),.out(B1mux_out),
+ pipeline_mux #(.Width(18),.RSTTYPE(RSTTYPE)) B1_mux(.in(mux_OPMODED4),.clk(CLK),.out(B1mux_out),
  .CE(CEB),.rst(RSTB),.sel(B1REG));
+ assign BCOUT=B1mux_out;
 
 wire [17:0] A1mux_out;
-pipeline_mux #(.Width(Width_A),.RSTTYPE(RSTTYPE)) A1_mux(.in(A0mux_out),.clk(CLK),.out(A1mux_out),
+pipeline_mux #(.Width(18),.RSTTYPE(RSTTYPE)) A1_mux(.in(A0mux_out),.clk(CLK),.out(A1mux_out),
 .CE(CEA),.rst(RSTA),.sel(A1REG));
-assign BCOUT=B1mux_out;
 
 wire [35:0] multiplier_out;
 assign multiplier_out= B1mux_out * A1mux_out;
-parameter Width_M=36;
-pipeline_mux #(.Width(Width_M),.RSTTYPE(RSTTYPE)) M_mux(.in(multiplier_out),.clk(CLK),.out(M),
+pipeline_mux #(.Width(36),.RSTTYPE(RSTTYPE)) M_mux(.in(multiplier_out),.clk(CLK),.out(M),
 .CE(CEM),.rst(RSTM),.sel(MREG));
-
-wire[47:0] M_mux_out;//output fur pipeline_mux Register M should be 48 bit 
 assign M_mux_out=multiplier_out;
 wire[47:0] Xmux_out;//output of mux x
  
- assign Xmux_out=(opmodemux_out[1:0]==2'b00)? 0 :(opmodemux_out[1:0]==2'b01)? M_mux_out:
+ assign Xmux_out=(opmodemux_out[1:0]==2'b00)? 0 :(opmodemux_out[1:0]==2'b01)? {12'b0,M}:
 (opmodemux_out[1:0]==2'b10)?PCOUT:{D[11:0],A[17:0],B[17:0]} ;// impelementation of Mux x
  
- parameter Width_c=18;
- wire[17:0] Cmux_out;//output of first stage of input C path
- pipeline_mux #(.Width(Width_c),.RSTTYPE(RSTTYPE)) C_mux(.in(C),.clk(CLK),.out(Cmux_out),.CE(CEC)
+ 
+ wire[47:0] Cmux_out;//output of first stage of input C path
+ pipeline_mux #(.Width(48),.RSTTYPE(RSTTYPE)) C_mux(.in(C),.clk(CLK),.out(Cmux_out),.CE(CEC)
 ,.rst(RSTC),.sel(CREG));
 
- wire[47:0] C_input_muxZ; //input to mux z should be 48 bit
  wire[47:0] Zmux_out;//output of Z mux
  assign Zmux_out=(opmodemux_out[3:2]==2'b00)? 0:(opmodemux_out[3:2]==2'b01)?PCIN:
-(opmodemux_out[3:2]==2'b10)? PCOUT :C_input_muxZ; //impelementation of MUX Z
+(opmodemux_out[3:2]==2'b10)? PCOUT :Cmux_out; //impelementation of MUX Z
 
 wire CY1;// input to register to select carry in or opmode5
-assign CY1 =(CARRYINSEL=="OPMODE5")? opmodemux_out[5] :CARRYIN;
+assign CY1 =(CARRYINSEL=="OPMODE5")? opmodemux_out[5] : (CARRYINSEL=="CARRYIN")? CARRYIN :0;
 
 wire CIN;//output of pipeline_mux and input to post adder substractor
-parameter Width_carryin=1;
- pipeline_mux #(.Width(Width_carryin),.RSTTYPE(RSTTYPE)) CARRYIN_mux(.in(CY1),.clk(CLK),.out(CIN),.CE(CECARRYIN)
+ pipeline_mux #(.Width(1),.RSTTYPE(RSTTYPE)) CARRYIN_mux(.in(CY1),.clk(CLK),.out(CIN),.CE(CECARRYIN)
 ,.rst(RSTCARRYIN),.sel(CARRYINREG));
 
 wire[47:0] ADDER2;// output of post adder substractor
 wire CYO;// carry out from post adder substractor
 assign {CYO,ADDER2} =(opmodemux_out[7])? Zmux_out - (Xmux_out + CIN) : Zmux_out + (Xmux_out + CIN);
-parameter Width_carryout=1;
-pipeline_mux #(.Width(Width_carryout),.RSTTYPE(RSTTYPE)) CARRYOUT_mux(.in(CYO),.clk(CLK),.out(CARRYOUT),
+pipeline_mux #(.Width(1),.RSTTYPE(RSTTYPE)) CARRYOUT_mux(.in(CYO),.clk(CLK),.out(CARRYOUT),
 .CE(CECARRYIN),.rst(RSTCARRYIN),.sel(CARRYOUTREG));
 assign CARRYOUTF=CARRYOUT;
-parameter Width_P=48;
-pipeline_mux #(.Width(Width_P),.RSTTYPE(RSTTYPE)) P_mux(.in(ADDER2),.clk(CLK),.out(PCOUT),
+pipeline_mux #(.Width(48),.RSTTYPE(RSTTYPE)) P_mux(.in(ADDER2),.clk(CLK),.out(PCOUT),
 .CE(CEP),.rst(RSTP),.sel(PREG));
 assign P=PCOUT;
  
--- DSP48A1/pipeline_mux.v
+++ DSP48A1/pipeline_mux.v
@@ -8,7 +8,7 @@ input clk,rst; // control signal for dsp (clk) and internal register(rst)
 input sel; // selection to select between seq or comb input
 input CE; // clock enable for internal register
 //output ports
-output  [Width-1:0] out ; // output after MUX
+output [Width-1:0] out ; // output after MUX
 reg[Width-1:0] in_reg; //output from register
 generate
     if(RSTTYPE=="SYNC") begin
